29fa0ffd510dae57af52674bd8a8f6283899fc4c
Wayne Mao <35285052+WayneMao@users.noreply.github.com>
2020-02-09 21:48:52 +0800

    spider

---
+++ spider/README.md
@@ -0,0 +1,9 @@
+# AI_Python_petproject
+Python small project
+
+python编程的一些练手小项目，主要有以下几个：
+
+
+
+- 21点小游戏
+
--- spider/TodayNews.py
+++ spider/TodayNews.py
@@ -1,4 +1,14 @@
 import numpy as np
 import json
+import requests
+import random
 
-def_
+def write_browser_info_to_file():
+    my_user_agent = requests.get()
+
+    with open("browser_info.json","w") as f:
+        json.dump(my_user_agent.text,f)
+
+write_browser_info_to_file()
+
+def 
